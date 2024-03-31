3dc005b1f16a05c65127c04dfc70dc7390125157
yashmikasewminimunasinghe <110669942+yashmikasewminimunasinghe@users.noreply.github.com>
2024-03-31 17:42:13 +0530

    new changes

---
+++ Data/DTO/VehicleModelDTO.cs
@@ -0,0 +1,11 @@
+﻿using FleetPulse_BackEndDevelopment.Models;
+
+namespace FleetPulse_BackEndDevelopment.Data.DTO
+{
+    public class VehicleModelDTO
+    {
+        public int VehicleModelId { get; set; }
+        public string Model { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Data/DTO/VehicleTypeDTO.cs
@@ -0,0 +1,11 @@
+﻿using FleetPulse_BackEndDevelopment.Models;
+
+namespace FleetPulse_BackEndDevelopment.Data.DTO
+{
+    public class VehicleTypeDTO
+    {
+        public int VehicleTypeId { get; set; }
+        public string Type { get; set; }
+        public bool Status { get; set; }
+    }
+}
